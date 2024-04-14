170dbcdd31265a1fa87139613ed95cf2a0006276
Harshad Vare <harshadv.we3tech@blinkx.in>
2024-04-14 22:23:59 +0530

    empty space

--- lib/features/counter_with_StateProvider/repository/counter_with_stateProvider_repository.dart
+++ lib/features/counter_with_StateProvider/repository/counter_with_stateProvider_repository.dart
@@ -1,3 +1,3 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final riverpodStateProvider = StateProvider<int>((ref) => 0);
+final riverpodStateProvider = StateProvider<int>((ref) => 0);  
